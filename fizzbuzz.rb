b8aa93262ba96d36418ea297657c4717f1aec64e
confident-token-1019 <theoonetj@gmail.com>
2020-01-21 02:03:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,4 +7,6 @@ def fizzbuzz(int)
   end
 end
 
-fizzbuzz(5)
+fizzbuzz(3) # => You should see a return of "Fizz"
+fizzbuzz(5) # => You should see a return of nil
+fizzbuzz()  # => You should get an ArgumentError
